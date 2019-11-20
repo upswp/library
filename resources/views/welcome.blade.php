0a78f35fab91ef34ae0ec6d7d0af1caa0d52510b
seolsun <loikii29@gmail.com>
2019-11-21 01:22:39 +0900

    깃허브 연동

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-        <title>Hi</title>
+        <title>Hello</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
