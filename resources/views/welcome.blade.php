61f217d2d33b39c902174dfa7657557eb7a984b9
seolsun <loikii29@gmail.com>
2019-11-21 19:10:26 +0900

    부트스트랩 연동

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,354 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="kor">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SMART LIBRARY</title>
+
+    <!--Bootstrap core CSS-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
 
-        <title>Hello</title>
+    <!--Custom Fonts-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- Custom styles for this template -->
+    <link href="css/custom.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/contact-input-style.css">
+    <link rel="stylesheet" href="css/hover-effect.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
 
-            .position-ref {
-                position: relative;
-            }
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+<nav class="navbar navbar-default top-bar affix" data-spy="affix" data-offset-top="250" >
+    <div class="container" >
+        <!- Brand and toggle get grouped for better mobile display ->
+        <div class="navbar-header page-scroll">
+            <button data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" class="navbar-toggle" type="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#home" class="navbar-brand">Cre8</a>
         </div>
-    </body>
+        <!- Collect the nav links, forms, and other content for toggling ->
+        <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li ><a href="#services">Services</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#features">Features</a></li>
+                <li><a href="#team">Team</a></li>
+                <li><a href="#">Sign Up</a></li>
+                <li><a href="#">Login</a></li>
+
+            </ul>
+        </div>
+        <!- /.navbar-collapse ->
+    </div>
+    <!- /.container-fluid ->
+</nav>
+
+<section class="banner-sec" id="home">
+<div class="container">
+<div class="jumbotron">
+<h1>도서 관리 프로그램</h1>
+<h2>원하는 책을 쉽게 찾아보세요.</h2>
+
+<div class="input-group">
+                <input type="text" placeholder="이곳에 입력하세요." class="form-control"><span class="input-group-btn">
+                <button type="button" class="btn btn-search">검색</button>
+                </span></div>
+</div>
+</div>
+</section>
+
+<section class="four-col-services" id="services">
+  <div class="container">
+    <div class="row">
+      <h2 class="text-center">Our Services</h2>
+      <h4 class="text-center">This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. </h4>
+
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block up-effect"><i class="fa fa-bar-chart" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div>
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block down-effect"><i class="fa fa-bookmark-o" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div>
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block up-effect"><i class="fa fa-bug" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div><br>
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block down-effect"><i class="fa fa-calendar-o" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div>
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block up-effect"><i class="fa fa-code" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div>
+
+      <div class="col-lg-4 col-md-4 col-sm-6 text-center service-block down-effect"><i class="fa fa-floppy-o" aria-hidden="true"></i><h3>Heading</h3><p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod.</p></div>
+
+
+
+    </div>
+  </div>
+</section>
+
+
+
+<section class="about-sec" id="about">
+<div class="container">
+<div class="row">
+<div class="col-md-7 col-sm-12 main-text"><h2>오시는 길</h2><p class="lead">This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. </p> <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumThis is Photoshop's versionof Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum</p>
+<p>Roin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumThis is Photoshop's versionof Lorem Ipsum.</p>
+ <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumThis is Photoshop's versionof Lorem Ipsum.</p>
+</div>
+<div class="col-md-5 col-sm-12 points">
+<div class="row">
+<div class="col-sm-6 col-xs-6 point-content text-center"><img src="img/icon-about-01.png"><p>less text here used to show less text. it works.</p></div>
+<div class="col-sm-6 col-xs-6 point-content text-center"><img src="img/icon-about-02.png"><p>less text here used to show less text. it works.</p></div>
+<div class="col-sm-6  col-xs-6 point-content text-center"><img src="img/icon-about-03.png"><p>less text here used to show less text. it works.</p></div>
+<div class="col-sm-6 col-xs-6 point-content text-center"><img src="img/icon-about-04.png"><p>less text here used to show less text. it works.</p></div>
+</div>
+</div>
+
+</div>
+</div>
+</section>
+
+<section class="feature-section" id="features">
+<div class="container">
+<h2 class="text-center">Features</h2>
+<p class="text-center sub-text">Proin gravida nibh vel velit auctor aliquet.</p>
+<div class="row">
+<div class="col-md-6 img-sec left-effect"><img class="img-responsive" src="img/info-bg-01.jpg"></div>
+<div class="col-md-6 right-effect"><div class="content-box"><h3>Basement Wireframe kit</h3><p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum</p>
+<a href="#">Know More<i class="fa fa-angle-right"></i></a></div></div>
+</div>
+
+<div class="row">
+<div class="col-md-6 col-md-push-6 img-sec left-effect"><img class="img-responsive" src="img/info-bg-03.jpg"></div>
+<div class="col-md-6 col-md-pull-6 right-effect"><div class="content-box"><h3>Basement Wireframe kit</h3><p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum</p>
+<a href="#">Know More<i class="fa fa-angle-right"></i></a></div></div>
+</div>
+
+<div class="row">
+<div class="col-md-6 img-sec left-effect"><img class="img-responsive" src="img/info-bg-02.jpg"></div>
+<div class="col-md-6 right-effect"><div class="content-box"><h3>Basement Wireframe kit</h3><p>This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum</p>
+<a href="#">Know More<i class="fa fa-angle-right"></i></a></div></div>
+</div>
+</div>
+</section>
+
+<!--<section class="team-sec" id="team">
+  <div class="container">
+    <div class="row">
+      <h2 class="text-center">Our Team<br><small>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat</small></h2>
+
+      <div class="col-lg-3 col-md-6 col-sm-6 text-center member-block"> <img class="up-effect" src="img/team-member-01.jpg">
+        <h3 class="up-effect">Harry Potter</h3>
+        <div class="contact-con down-effect"><i class="fa fa-phone fa-2x"></i><i class="fa fa-envelope-o fa-2x"></i><i class="fa fa-heart-o fa-2x"></i></div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 text-center member-block"> <img class="up-effect" src="img/team-member-02.jpg">
+        <h3 class="up-effect">Ron Weasly</h3>
+        <div class="contact-con down-effect"><i class="fa fa-phone fa-2x"></i><i class="fa fa-envelope-o fa-2x"></i><i class="fa fa-heart-o fa-2x"></i></div>
+              </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 text-center member-block"> <img class="up-effect" src="img/team-member-03.jpg">
+        <h3 class="up-effect">Harmion Granger</h3>
+                <div class="contact-con down-effect"><i class="fa fa-phone fa-2x"></i><i class="fa fa-envelope-o fa-2x"></i><i class="fa fa-heart-o fa-2x"></i></div>
+
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 text-center member-block"> <img class="up-effect" src="img/team-member-04.jpg">
+        <h3 class="up-effect">Longbottom</h3>
+                <div class="contact-con down-effect"><i class="fa fa-phone fa-2x"></i><i class="fa fa-envelope-o fa-2x"></i><i class="fa fa-heart-o fa-2x"></i></div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!-- <section class="testimonial-sec" id="testimonials">
+<div class="container">
+<div class="row">
+<div class="col-md-12 text-center"><h2>Client Testimonials</h2></div>
+<div class="col-md-10 col-md-offset-1">
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators --
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides --
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+
+      <div class="profile-circle text-center"><img src="img/client-01.jpg"></div>
+              <blockquote>
+                <p><em>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus."</em></p>
+                <footer>Someone famous in Source Title</footer>
+              </blockquote>
+
+    </div>
+    <div class="item">
+      <div class="profile-circle text-center"><img src="img/client-02.jpg"></div>
+              <blockquote>
+                <p><em>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus."</em></p>
+                <footer>Someone famous in Source Title</footer>
+              </blockquote>
+
+    </div>
+    <div class="item">
+      <div class="profile-circle text-center"><img src="img/client-03.jpg"></div>
+              <blockquote>
+                <p><em>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, veritatis nulla eum laudantium totam tempore optio doloremque laboriosam quas, quos eaque molestias odio aut eius animi. Impedit temporibus nisi accusamus."</em></p>
+                <footer>Someone famous in Source Title</footer>
+              </blockquote>
+
+    </div>
+  </div>
+
+</div>
+</div>
+</div>
+
+</div>
+
+</section> -->
+
+<div class="f-small-block">
+<div class="container">
+<div class="row small-inner text-center">
+<h3>Subscribe our newsletter</h3>
+<p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsumThis is Photoshop's versionof Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum</p>
+<!--<a href="#" class="btn btn-danger btn-capsul">Subscribe Now</a>-->
+</div>
+</div>
+</div>
+
+
+<!-- <section class="content contact-sec" id="contact">
+				<div class="container">
+                <div class="row">
+                <div class="col-lg-12 text-center"><h2>Contact Us</h2></div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Name</span>
+					</label>
+				</span>
+                </div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Phone</span>
+					</label>
+				</span>
+                </div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+                <span class="input input-hoshi">
+					<input class="input_field input_field-hoshi" type="text" id="input-4" />
+					<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Email</span>
+					</label>
+				</span>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+                <span class="t-area input-hoshi">
+                <textarea class="input_field input_field-hoshi" rows="1"></textarea>
+						<label class="input_label input_label-hoshi input_label-hoshi-color-1" for="input-4">
+						<span class="input_label-content input_label-content-hoshi">Email</span>
+					</label>
+				</span>
+                <a href="#" class="btn btn-submit btn-capsul">Submit</a>
+                </div>
+
+                </div>
+                </div>
+
+</section> -->
+<footer>
+<div class="container">
+<div class="row">
+<p class="text-center">Shared by <i class="fa fa-love"></i><a href="https://bootstrapthemes.co">BootstrapThemes</a>
+</p>
+</div>
+</div>
+</footer>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/oppear_1.1.2.min.js"></script>
+    <script>
+
+	$(window).scroll(function () {
+		var sticky = $('.navbar-brand'),
+		    scroll = $(window).scrollTop();
+
+			if (scroll >= 250) sticky.addClass('dark');
+			else sticky.removeClass('dark');
+
+	});
+
+	$('.service-box').Oppear({
+		delay : 500,
+		});
+	$('.main-text').Oppear({
+		direction:'right',
+	});
+	$('.points').Oppear({
+		direction:'left',
+	});
+	$('.up-effect').Oppear({
+		direction:'up',
+	});
+	$('.down-effect').Oppear({
+		direction:'down',
+	});
+	$('.left-effect').Oppear({
+		direction:'right',
+	});
+	$('.right-effect').Oppear({
+		direction:'left',
+	});
+
+
+
+    </script>
+    <script>
+	$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+    </script>
+  </body>
 </html>
