d1f2f765fa8043e0aa924e50fbbb78359a886840
seolsun <loikii29@gmail.com>
2019-11-22 21:52:05 +0900

    테스트2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,7 @@
         <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li ><a href="#services">프로그램 소개</a></li>
-                <li><a href="#about">이용 안내</a></li>
+                <li><a href="#about">이용 안내예요</a></li>
                 <li><a href="#features">공지 사항</a></li>
                 <li><a href="#team">도서 검색</a></li>
                 <!--<li><a href="#">MYPAGE</a></li>
