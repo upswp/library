c4e27ff9a3457222a6ed05e13f4d59233ccd0d15
seolsun <loikii29@gmail.com>
2019-11-24 19:07:25 +0900

    레이아웃 작업 시작

---
+++ resources/views/layouts/quicmenu.blade.php
@@ -0,0 +1,6 @@
+<!-- quick menu 버튼 모음 -->
+<div class="btn-group-vertical" data-toggle="buttons">
+    <button type="button" class="btn btn-btn1 hover4">MYPAGE</button>
+    <button type="button" class="btn btn-btn3 hover4"><a href=#signin>SIGN-IN</a></button>
+
+</div>
---
+++ resources/views/topmenu.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-default top-bar affix" data-spy="affix" data-offset-top="250" >
+    <div class="container" >
+        <!- Brand and toggle get grouped for better mobile display ->
+        <div class="navbar-header page-scroll">
+            <button data-target="#bs-example-navbar-collapse-1" data-toggle="collapse" class="navbar-toggle" type="button">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="#home" class="navbar-brand">Cre8</a>
+        </div>
+        <!- Collect the nav links, forms, and other content for toggling ->
+        <div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li ><a href="#services">프로그램 소개</a></li>
+                <li><a href="#about">이용 안내</a></li>
+                <li><a href="#features">공지 사항</a></li>
+                <li><a href="#team">도서 검색</a></li>
+                <!--<li><a href="#">MYPAGE</a></li>
+                <li><a href="#">LOGIN</a></li>-->
+
+            </ul>
+        </div>
+        <!- /.navbar-collapse ->
+    </div>
+    <!- /.container-fluid ->
+</nav>
