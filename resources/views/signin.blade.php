39e08d6b6df40dabbc6f9b1a5349e71cb2d051cd
seolsun <loikii29@gmail.com>
2019-11-24 18:57:07 +0900

    로그인 연동

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -8,27 +8,27 @@
 </head>
 <body>
 
-<section class="container">
+<section class="container" >
 		    <article class="half">
-			        <h1>Azure</h1>
+			        <h1>Cre8</h1>
 			        <div class="tabs">
 				            <span class="tab signin active"><a href="#signin">Sign in</a></span>
 				            <span class="tab signup"><a href="#signup">Sign up</a></span>
 			        </div>
 			        <div class="content">
 				            <div class="signin-cont cont">
-					                <form action="#" method="post" enctype="multipart/form-data">
-						                    <input type="email" name="email" id="email" class="inpt" required="required" placeholder="Your email">
-						                    <label for="email">Your email</label>
-						                    <input type="password" name="password" id="password" class="inpt" required="required" placeholder="Your password">
-                						    <label for="password">Your password</label>
-						                    <input type="checkbox" id="remember" class="checkbox" checked>
-						                    <label for="remember">Remember me</label>
-						                    <div class="submit-wrap">
-							                        <input type="submit" value="Sign in" class="submit">
-							                        <a href="#" class="more">Forgot your password?</a>
-						                    </div>
-        					        </form>
+                                <form action="#" method="post" enctype="multipart/form-data">
+                                    <input type="email" name="email" id="email" class="inpt" required="required" placeholder="Your email">
+                                    <label for="email">Your email</label>
+                                    <input type="password" name="password" id="password" class="inpt" required="required" placeholder="Your password">
+                                    <label for="password">Your password</label>
+                                    <input type="checkbox" id="remember" class="checkbox" checked>
+                                    <label for="remember">Remember me</label>
+                                    <div class="submit-wrap">
+                                            <input type="submit" value="Sign in" class="submit">
+                                            <a href="#" class="more">Forgot your password?</a>
+                                    </div>
+                            </form>
     				        </div>
     				        <div class="signup-cont cont">
                 <form action="#" method="post" enctype="multipart/form-data">
